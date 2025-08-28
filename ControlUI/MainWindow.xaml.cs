2dacbb1ed17091664029433e7a09045154cbf6f3
Anthogr <87010226+Anthogr@users.noreply.github.com>
2025-08-28 13:56:42 +0200

    Update UI

--- ControlUI/MainWindow.xaml.cs
+++ ControlUI/MainWindow.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace ControlUI
+namespace MachineControlUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ModbusClient _modbus;
+
         public MainWindow()
         {
             InitializeComponent();
+            _modbus = new ModbusClient("192.168.0.100", 502); // exemple
+        }
+
+        private void BtnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            _modbus.SetModeAuto();
+            TxtGemma.Text = "GEMMA mode: Auto";
+        }
+
+        private void BtnManual_Click(object sender, RoutedEventArgs e)
+        {
+            _modbus.SetModeManual();
+            TxtGemma.Text = "GEMMA mode: Manuel";
+        }
+
+        private void BtnInit_Click(object sender, RoutedEventArgs e)
+        {
+            _modbus.StartInitCycle();
+            TxtGemma.Text = "GEMMA mode: Initialisation";
         }
     }
 }
