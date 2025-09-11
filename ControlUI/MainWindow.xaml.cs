73101284e5c95e32bccffc1455f1f9b09f5cc2c9
Anthogr <87010226+Anthogr@users.noreply.github.com>
2025-09-11 16:23:37 +0200

    Bug fix

--- ControlUI/MainWindow.xaml.cs
+++ ControlUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Controls;
 using System.Windows.Threading;
 using System.Threading.Tasks;
 using ModbusTCP_Simplified;
